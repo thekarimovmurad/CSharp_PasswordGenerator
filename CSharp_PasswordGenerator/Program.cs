1be735ef41acfbe1c52cec93a4fe90b6c72c2eae
thekarimovmurad <100101932+thekarimovmurad@users.noreply.github.com>
2023-01-14 01:48:36 +0100

    Add project files.

---
+++ CSharp_PasswordGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PasswordLenght = Convert.ToInt32(Console.ReadLine());
+            string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%+-*/.,";
+
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < PasswordLenght--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
